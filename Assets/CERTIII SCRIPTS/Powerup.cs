b4551828637c25ee020e35527e28c2ab52e35726
Mitchell Local <idiovoidi@gmail.com>
2026-06-04 19:20:11 +1000

    Powerup Draft script

--- Assets/CERTIII SCRIPTS/Powerup.cs
+++ Assets/CERTIII SCRIPTS/Powerup.cs
@@ -3,12 +3,26 @@ using UnityEngine;
 public class Powerup : MonoBehaviour
 {
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// 
+    // Value of the Powerup's effect
+    private int PowerupValue = 25;
+    // Seconds the powerup is active
+    private int PowerupLifetime = 15;
+    
     /// YOU NEED A VARIABLE FOR THE AMOUNT YOU ARE CHANGING AND ONE FOR THE AMOUNT OF
     /// TIME THE POWERUP IS ACTIVE FOR (POWERUP DEPENDING) 
     /// set up an OnTriggerEnter function
     /// check if the collider has the "Player" tag
+// Trigger for Character component
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Character>() != null)
+        {
+            // Adds overhealth to the Character, allowing health to exceed maximum
+            collider.gameObject.GetComponent<Character>().ChangeCurrentHealth(PowerupValue);
+            // Destroys object 
+            Destroy(gameObject);
+        }
+    }
     /// if it does access:
     /// collider.gameObject.GetComponent<Character>() FOR CHARACTER POWERUPS
     /// collider.gameObject.GetComponent<Character>().equippedWeapon FOR WEAPON POWERUPS
